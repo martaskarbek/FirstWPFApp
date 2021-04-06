e60405753322bd11e355df11b874ef8866ee122d
martaskarbek <48453376+martaskarbek@users.noreply.github.com>
2021-04-06 02:03:26 +0200

    tests

--- FirstWPFApp/FirstWPFApp/MainWindow.xaml.cs
+++ FirstWPFApp/FirstWPFApp/MainWindow.xaml.cs
@@ -21,15 +21,45 @@ namespace FirstWPFApp
     public partial class MainWindow : Window
     {
         public int Counter { get; private set; }
+        public string[] text = new string[4]{"lalala", "tralala", "okokokok", "hmhmhmhm" };
+        InkEditingModes editingModes = new InkEditingModes();
         public MainWindow()
         {
             InitializeComponent();
+
+           // this.DataContext = editingModes;
+
             Counter = 0;
         }
 
         private void first_Click(object sender, RoutedEventArgs e)
         {
-            onEvent.Content = Counter++;
+            // onEvent.Content = Counter++;
+            Random r = new Random();
+            int a = r.Next(0, 4);
+            MessageBox.Show(text[a]);
         }
+
+        private void Selection_Click(object sender, RoutedEventArgs e)
+        {
+            (Application.Current.Resources["editingModes"] as InkEditingModes).EditingMode = InkCanvasEditingMode.Select;
+            // editingModes.EditingMode = InkCanvasEditingMode.Select;
+        }
+
+        private void Drawing_Click(object sender, RoutedEventArgs e)
+        {
+            (Application.Current.Resources["editingModes"] as InkEditingModes).EditingMode = InkCanvasEditingMode.Ink;
+         //   editingModes.EditingMode = InkCanvasEditingMode.Ink;
+        }
+
+        /*private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            secondary.Content = "Clicked";
+        }*/
+
+        /*private void Change_Click(object sender, RoutedEventArgs e)
+        {
+            text_block.Text = "Changed text";
+        }*/
     }
 }
